5b43fc2169341cddaef08ba05b2704f823aa05c4
farofeiro231 <victoremanoel23@gmail.com>
2018-02-07 20:29:04 +0100

    Fixed the problem changing file from .mp3 to .wav

--- sound_test/sound.py
+++ sound_test/sound.py
@@ -1,8 +1,9 @@
-import pygame
+import pygame, sys
+from pygame.locals import *
 import time
 
 pygame.init()
-soundObj = pygame.mixer.Sound('noo.mp3')
+soundObj = pygame.mixer.Sound('beep-01a.wav')
 soundObj.play()
-time.sleep(1.5)
+time.sleep(0.5)
 soundObj.stop()
