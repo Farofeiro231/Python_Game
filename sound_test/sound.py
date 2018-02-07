2f8149afbaaa13e7939352401edb856ac4b5cb44
farofeiro231 <victoremanoel23@gmail.com>
2018-02-07 20:32:22 +0100

    Added a background noise from a .mp3 file

--- sound_test/sound.py
+++ sound_test/sound.py
@@ -1,9 +1,13 @@
-import pygame, sys
-from pygame.locals import *
-import time
+import pygame, time
 
 pygame.init()
 soundObj = pygame.mixer.Sound('beep-01a.wav')
 soundObj.play()
-time.sleep(0.5)
+
+pygame.mixer.music.load('noo.mp3') #plays the backgournd sound
+pygame.mixer.music.play(-1, 0.0) #plays it forever (-1) and from the beginning (0.0)
+
+time.sleep(10)
+
 soundObj.stop()
+pygame.mixer.music.stop()
