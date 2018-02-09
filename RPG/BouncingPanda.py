2fd1d4985cd406110ebceae7c94fdd1612c02a36
farofeiro231 <victoremanoel23@gmail.com>
2018-02-09 11:14:54 +0100

    Changed names from panda to hero

--- RPG/BouncingPanda.py
+++ RPG/BouncingPanda.py
@@ -4,22 +4,19 @@ from pygame.locals import *
 FPS = 30
 WINWIDTH = 640
 WINHEIGHT = 480
-PANDAWORLDX = 10
-PANDAWORLDY = 10
-PANDA_CAMX = 10
-PANDA_CAMY = 10
-PIGX = 100
-PIGY = 10
+HEROWORLDX = 10
+HEROWORLDY = 10
+HERO_CAMX = 10
+HERO_CAMY = 10
 SIZE = 80
-panda_face = 'right'
-pig_face = 'right'
+hero_face = 'right'
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 BAMBOO = (24, 255, 0)
 
 '''
-Execution of the code of the jumping panda. Pre-configuration of the game window.
+Execution of the code of the jumping hero. Pre-configuration of the game window.
 '''
 pygame.init()
 
@@ -27,61 +24,49 @@ pygame.display.set_icon(pygame.image.load('images/super_panda.png')) #Changes th
 fpsClock = pygame.time.Clock()
 
 DISPLAYSURF = pygame.display.set_mode((WINWIDTH, WINHEIGHT))
-pygame.display.set_caption('Jumping Panda')
+pygame.display.set_caption('Jumping Hero')
 
-PANDA = {'surface': pygame.image.load('images/hero.png'),
-         'worldX': PANDAX,
-         'worldY': PANDAY,
-         'camX': PANDA_CAMX,
-         'camY': PANDA_CAMY}
-scaledPanda = pygame.transform.scale(PANDA['surface'], (SIZE, SIZE))
-
-PIG = pygame.image.load('images/super_pig.png')
-scaledPig = pygame.transform.scale(PIG, (SIZE, SIZE))
+HERO = {'surface': pygame.image.load('images/hero.png'),
+         'worldX': HEROWORLDX,
+         'worldY': HEROWORLDY,
+         'camX': HERO_CAMX,
+         'camY': HERO_CAMY}
+scaledHero = pygame.transform.scale(HERO['surface'], (SIZE, SIZE))
 
 pygame.mixer.music.load('sounds/triumph.wav') #plays the backgournd sound
 pygame.mixer.music.play(-1, 0.0) #plays it forever (-1) and from the beginning (0.0)
 
 while True:
     DISPLAYSURF.fill(BAMBOO)
-    DISPLAYSURF.blit(scaledPanda, (PANDA_CAMX, PANDA_CAMY))
-    #DISPLAYSURF.blit(scaledPig, (PIGX, PIGY))
+    DISPLAYSURF.blit(scaledHero, (HERO_CAMX, HERO_CAMY))
     event = pygame.key.get_pressed()
         #if event.type == KEYDOWN:
     if event[pygame.K_UP] or event[pygame.K_w]:
-        PANDAY -= 5
-        PANDA_CAMY -= 5
-        PIGY -= 5
-        if PANDA_CAMY <= -SIZE:
-            PANDA_CAMY = WINHEIGHT
+        HEROWORLDY -= 5
+        HERO_CAMY -= 5
+        if HERO_CAMY <= -SIZE:
+            HERO_CAMY = WINHEIGHT
     if event[pygame.K_LEFT] or event[pygame.K_a]:
-        PANDAX -= 5
-        PANDA_CAMX -= 5
-        PIGX -= 5
-        if panda_face == 'right':
-            scaledPig = pygame.transform.flip(scaledPig, True, False)
-            scaledPanda = pygame.transform.flip(scaledPanda, True, False)
-            panda_face = 'left'
-            pig_face = 'left'
-        if PANDA_CAMX <= -SIZE:
-            PANDA_CAMX = WINWIDTH
+        HEROWORLDX -= 5
+        HERO_CAMX -= 5
+        if hero_face == 'right':
+            scaledHero = pygame.transform.flip(scaledHero, True, False)
+            hero_face = 'left'
+        if HERO_CAMX <= -SIZE:
+            HERO_CAMX = WINWIDTH
     if event[pygame.K_RIGHT] or event[pygame.K_d]:
-        PANDAX += 5
-        PANDA_CAMX += 5
-        PIGX += 5
-        if panda_face == 'left':
-            scaledPig = pygame.transform.flip(scaledPig, True, False)
-            scaledPanda = pygame.transform.flip(scaledPanda, True, False)
-            panda_face = 'right'
-            pig_face = 'right'
-        if PANDA_CAMX >= WINWIDTH:
-            PANDA_CAMX = 0 - SIZE
+        HEROWORLDX += 5
+        HERO_CAMX += 5
+        if hero_face == 'left':
+            scaledHero = pygame.transform.flip(scaledHero, True, False)
+            hero_face = 'right'
+        if HERO_CAMX >= WINWIDTH:
+            HERO_CAMX = 0 - SIZE
     if event[pygame.K_DOWN] or event[pygame.K_s]:
-        PANDAY += 5
-        PANDA_CAMY += 5
-        PIGY += 5
-        if PANDA_CAMY >= WINHEIGHT:
-            PANDA_CAMY = 0 - SIZE
+        HEROWORLDY += 5
+        HERO_CAMY += 5
+        if HERO_CAMY >= WINHEIGHT:
+            HERO_CAMY = 0 - SIZE
     for evento in pygame.event.get():
         if evento.type == QUIT:
             pygame.mixer.music.stop()
