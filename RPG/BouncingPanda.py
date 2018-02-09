307dc7ef323009c9df73d76897ced720465fea2c
farofeiro231 <victoremanoel23@gmail.com>
2018-02-09 12:53:08 +0100

    Added coordinates for the center of the figure

--- RPG/BouncingPanda.py
+++ RPG/BouncingPanda.py
@@ -4,14 +4,16 @@ from pygame.locals import *
 FPS = 30
 WINWIDTH = 640
 WINHEIGHT = 480
-HEROWORLDX = 10
-HEROWORLDY = 10
+SIZE = 80
+HEROWORLDX = WINWIDTH/2
+HEROWORLDY = WINHEIGHT/2
+CENTER_HEROX = HEROWORLDX - SIZE/2
+CENTER_HEROY = HEROWORLDY - SIZE/2
 HERO_CAMX = 10
 HERO_CAMY = 10
 CAMX = 0
 CAMY = 0
 CAM_OFFSET = 90
-SIZE = 80
 hero_face = 'right'
 
 BLACK = (0, 0, 0)
@@ -30,44 +32,40 @@ DISPLAYSURF = pygame.display.set_mode((WINWIDTH, WINHEIGHT))
 pygame.display.set_caption('Jumping Hero')
 
 HERO = {'surface': pygame.image.load('images/hero.png'),
-         'worldX': HEROWORLDX,
-         'worldY': HEROWORLDY}
+         'worldX': CENTER_HEROX,
+         'worldY': CENTER_HEROY}
 scaledHero = pygame.transform.scale(HERO['surface'], (SIZE, SIZE))
 
 pygame.mixer.music.load('sounds/triumph.wav') #plays the backgournd sound
 pygame.mixer.music.play(-1, 0.0) #plays it forever (-1) and from the beginning (0.0)
 
 while True:
-    HERO_CAMX = HEROWORLDX - CAMX
-    HERO_CAMY = HEROWORLDY - CAMY
+    HERO_CAMX = CENTER_HEROX - CAMX
+    HERO_CAMY = CENTER_HEROY - CAMY
     DISPLAYSURF.fill(BAMBOO)
     DISPLAYSURF.blit(scaledHero, (HERO_CAMX, HERO_CAMY))
     event = pygame.key.get_pressed()
         #if event.type == KEYDOWN:
     if event[pygame.K_UP] or event[pygame.K_w]:
-        HEROWORLDY -= 5
-        CAMY -= 5
-        if CAMY <= -SIZE:
-            CAMY = WINHEIGHT
+        CENTER_HEROY -= 5
+        if CENTER_HEROY >= CAMY + WINHEIGHT + CAM_OFFSET:
+            CAMY = CAMY + CAM_OFFSET
     if event[pygame.K_LEFT] or event[pygame.K_a]:
-        HEROWORLDX -= 5
-        CAMX -= 5
+        CENTER_HEROX -= 5
         if hero_face == 'right':
             scaledHero = pygame.transform.flip(scaledHero, True, False)
             hero_face = 'left'
-        if CAMX <= -SIZE:
+        if CENTER_HEROX <= -SIZE:
             CAMX = WINWIDTH
     if event[pygame.K_RIGHT] or event[pygame.K_d]:
-        HEROWORLDX += 5
-        CAMX += 5
+        CENTER_HEROX += 5
         if hero_face == 'left':
             scaledHero = pygame.transform.flip(scaledHero, True, False)
             hero_face = 'right'
         if CAMX >= WINWIDTH:
             CAMX = 0 - SIZE
     if event[pygame.K_DOWN] or event[pygame.K_s]:
-        HEROWORLDY += 5
-        CAMY += 5
+        CENTER_HEROY += 5
         if CAMY >= WINHEIGHT:
             CAMY = 0 - SIZE
     for evento in pygame.event.get():
